f323f815426a46f8c857559e7166c8e2dff92e8d
Jeff Lindsay <progrium@gmail.com>
2013-11-07 01:15:38 -0800

    fixing trailing slash

--- viewdocs.go
+++ viewdocs.go
@@ -29,10 +29,13 @@ func parseRequest(r *http.Request) (user, repo, doc string, err error) {
 	user = hostname[0]
 	path := strings.Split(r.RequestURI, "/")
 	repo = path[1]
-	if len(path) < 3 {
+	if len(path) < 3 || (len(path) == 3 && strings.HasSuffix(r.RequestURI, "/")) {
 		doc = "index"
 	} else {
 		doc = strings.Join(path[2:], "/")
+		if strings.HasSuffix(doc, "/") {
+        	doc = doc[:len(doc)-1]
+    	}
 	}
 	return
 }
