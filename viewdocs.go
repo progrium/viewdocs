3231d21dde5c32503b02ec1351b5a78f5a26e21f
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-02-17 02:28:37 -0500

    Add support for PAGE_CLASSES. Closes #30

--- viewdocs.go
+++ viewdocs.go
@@ -152,10 +152,14 @@ func fetchAndRenderDoc(user, repo, ref, doc string) (string, error) {
 		return "", err
 	}
 
+	pagesClasses := strings.Replace(doc, "/", "-", -1)
+	pagesClasses = pagesClasses[:len(pagesClasses)-len(path.Ext(pagesClasses))]
+
 	output := strings.Replace(<-template, "{{CONTENT}}", string(body), 1)
 	templateRecv = true
 	output = strings.Replace(output, "{{NAME}}", repo, -1)
 	output = strings.Replace(output, "{{USER}}", user, -1)
+	output = strings.Replace(output, "{{PAGE_CLASSES}}", pagesClasses, -1)
 
 	// Fix relative links
 	output, err = fixRelativeLinks(doc, repo, ref, output)
